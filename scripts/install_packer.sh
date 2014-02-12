6fadf3d5c9095a70ccad5aee7b39909f55fe86da
Ryan Skoblenick <ryan@skoblenick.com>
2014-02-12 04:38:19 -0500

    Added Linux support to Packer install script

--- scripts/install_packer.sh
+++ scripts/install_packer.sh
@@ -5,8 +5,9 @@ sudo -v
 
 function install () {
     version=$1
+    platform=$2
 
-    curl -L -o "/tmp/packer-${version}" --url "https://dl.bintray.com/mitchellh/packer/${version}_darwin_amd64.zip"
+    curl -L -o "/tmp/packer-${version}" --url "https://dl.bintray.com/mitchellh/packer/${version}_${platform}_amd64.zip"
     sudo mkdir -p /usr/local/packer
     sudo unzip "/tmp/packer-${version}" -d /usr/local/packer
     rm -rf "/tmp/packer-${version}"
@@ -19,7 +20,15 @@ function is_installed () {
 function main () {
 	is_installed "curl"
 	is_installed "unzip"
-    install "0.5.1"
+
+	if [[ $OSTYPE == "darwin"* ]]; then
+    	install "0.5.1" "darwin"
+    elif [[ $OSTYPE == "linux"* ]]; then
+    	install "0.5.1" "linux"
+    else
+    	echo "Unsupported OS. Aborting"
+    	exit 2
+    fi
 }
 
 main
