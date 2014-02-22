72a1eb6574f6b068b4c0ad54c565215104b82508
Ryan Skoblenick <ryan@skoblenick.com>
2014-02-22 05:35:37 -0500

    Updated packer installation script
    - Fixed spacing in script
    - Added overwrite existing files to unzip command

--- scripts/install_packer.sh
+++ scripts/install_packer.sh
@@ -9,25 +9,25 @@ function install () {
 
     curl -L -o "/tmp/packer-${version}" --url "https://dl.bintray.com/mitchellh/packer/${version}_${platform}_amd64.zip"
     sudo mkdir -p /usr/local/packer
-    sudo unzip "/tmp/packer-${version}" -d /usr/local/packer
+    sudo unzip -o "/tmp/packer-${version}" -d /usr/local/packer
     rm -rf "/tmp/packer-${version}"
 }
 
 function is_installed () {
-	type "${1}" >/dev/null 2>&1 || { echo >&2 "'${1}' is require and not installed. Aborting."; exit 1; }
+    type "${1}" >/dev/null 2>&1 || { echo >&2 "'${1}' is require and not installed. Aborting."; exit 1; }
 }
 
 function main () {
-	is_installed "curl"
-	is_installed "unzip"
+    is_installed "curl"
+    is_installed "unzip"
 
-	if [[ $OSTYPE == "darwin"* ]]; then
-    	install "0.5.1" "darwin"
+    if [[ $OSTYPE == "darwin"* ]]; then
+        install "0.5.2" "darwin"
     elif [[ $OSTYPE == "linux"* ]]; then
-    	install "0.5.1" "linux"
+        install "0.5.2" "linux"
     else
-    	echo "Unsupported OS. Aborting"
-    	exit 2
+        echo "Unsupported OS. Aborting"
+        exit 2
     fi
 }
 
